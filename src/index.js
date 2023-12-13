78caf783d4cfd352912d2f5304d7fc9ee348919b
Artem Kalach <rtmklcwork@gmail.com>
2023-12-13 08:16:27 +0300

    persistAdded

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from './store/store';
+import store, { persister } from './store/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persister}>
+        <App />
+      </PersistGate>
+    </Provider>
   </BrowserRouter>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,25 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from '../slices/userSlice'
 import { coinCapApi } from '../components/coins/Coins';
 import coinsReducer from '../slices/coinsSlice'
 import walletReducer from '../slices/walletSlice'
 
+const persistConfig = {
+    key: 'root',
+    storage,
+}
 
 const rootReducer = combineReducers({
     user: userReducer,
@@ -11,9 +27,15 @@ const rootReducer = combineReducers({
     wallet: walletReducer,
     [coinCapApi.reducerPath]: coinCapApi.reducer,
 })
-
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(coinCapApi.middleware),
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoreActions: [FLUSH, REHYDRATE, REGISTER, PAUSE, PERSIST, PURGE]
+        }
+    }).concat(coinCapApi.middleware),
 })
+
+export const persister = persistStore(store)
 export default store;
